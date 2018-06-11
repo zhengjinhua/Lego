a4606d9dc8a826ef71d588e9ac843dd68ff47c33
jh <jinhua1023@163.com>
2018-06-11 15:35:54 +0800

    improvement

--- Lego/Plugin/InjectionFilter/Plugin.php
+++ Lego/Plugin/InjectionFilter/Plugin.php
@@ -19,6 +19,8 @@ use Core\PluginInterface;
 class Plugin implements PluginInterface
 {
 
+    private static $ignoreKeyArr = ["contents","template"];
+
     //get拦截规则
     private static $getfilter = "\\<.+javascript:window\\[.{1}\\\\x|<.*=(&#\\d+?;?)+?>|<.*(data|src)=data:text\\/html.*>|\\b(alert\\(|confirm\\(|expression\\(|prompt\\(|benchmark\s*?\(.*\)|sleep\s*?\(.*\)|load_file\s*?\\()|<[a-z]+?\\b[^>]*?\\bon([a-z]{4,})\s*?=|^\\+\\/v(8|9)|\\b(and|or)\\b\\s*?([\\(\\)'\"\\d]+?=[\\(\\)'\"\\d]+?|[\\(\\)'\"a-zA-Z]+?=[\\(\\)'\"a-zA-Z]+?|>|<|\s+?[\\w]+?\\s+?\\bin\\b\\s*?\(|\\blike\\b\\s+?[\"'])|\\/\\*.*\\*\\/|<\\s*script\\b|\\bEXEC\\b|UNION.+?SELECT\s*(\(.+\)\s*|@{1,2}.+?\s*|\s+?.+?|(`|'|\").*?(`|'|\")\s*)|UPDATE\s*(\(.+\)\s*|@{1,2}.+?\s*|\s+?.+?|(`|'|\").*?(`|'|\")\s*)SET|INSERT\\s+INTO.+?VALUES|(SELECT|DELETE)@{0,2}(\\(.+\\)|\\s+?.+?\\s+?|(`|'|\").*?(`|'|\"))FROM(\\(.+\\)|\\s+?.+?|(`|'|\").*?(`|'|\"))|(CREATE|ALTER|DROP|TRUNCATE)\\s+(TABLE|DATABASE)";
     //post拦截规则
@@ -33,13 +35,13 @@ class Plugin implements PluginInterface
     {
         Event::attach('CORE.REQUEST.INIT', function () {
 
-            foreach ($_GET as $key => &$value) {
+            foreach ($_GET as $key => $value) {
                 self::stopAttack($key, $value, self::$getfilter);
             }
-            foreach ($_POST as $key => &$value1) {
+            foreach ($_POST as $key => $value1) {
                 self::stopAttack($key, $value1, self::$postfilter);
             }
-            foreach ($_COOKIE as $key => &$value2) {
+            foreach ($_COOKIE as $key => $value2) {
                 self::stopAttack($key, $value2, self::$cookiefilter);
             }
         });
@@ -52,7 +54,7 @@ class Plugin implements PluginInterface
      * @param $arrFiltReq
      * @throws
      */
-    private static function stopAttack($strFiltKey, &$strFiltValue, $arrFiltReq)
+    private static function stopAttack($strFiltKey, $strFiltValue, $arrFiltReq)
     {
         $strFiltValueStr = $strFiltKey . self::arrForeach($strFiltValue);
         if (preg_match("/" . $arrFiltReq . "/is", $strFiltValueStr) == 1) {
@@ -65,31 +67,22 @@ class Plugin implements PluginInterface
      * @param $arr
      * @return string
      */
-    private static function arrForeach(&$arr)
+    private static function arrForeach($arr)
     {
-        static $str;
+        $str = '';
 
         if (!is_array($arr)) {
             return $arr;
         }
 
-        $ignoreKeyArr = [];
-
-        foreach ($arr as $key => &$val) {
-            if(in_array($key,$ignoreKeyArr)){
-                continue;
-            }
+        foreach ($arr as $key => $val) {
             if (is_array($val)) {
-                self::arrForeach($val);
+                $str[] = self::arrForeach($val);
             } else {
-                $val = trim($val);
-                if (strpos($key, '-XSSIGNORE') > 0) {
-                    $ignoreKeyArr[] = $newKey = substr($key, 0, strpos($key, '-XSSIGNORE'));
-                    $arr[$newKey] = $val;
-                    unset($arr[$key]);
+                if(in_array($key,self::$ignoreKeyArr)){
                     continue;
                 }
-
+                $val = trim($val);
                 $str[] = $key . $val;
             }
         }
